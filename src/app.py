433e422db0af2c27dfd8e3e7a5d34a0de3b32f47
Umut Topalak <umuttopalak@hotmail.com>
2024-10-16 21:31:55 +0300

    Refactor app.py to use dynamic port for deployment

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def healthcheck():
+@app.route('/')
+def hello():
     return {"data": "Hello Backend"}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
